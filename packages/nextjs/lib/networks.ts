e56a0d3e5f543ee68a725447d6ac93b08ab7867c
samueldanso <me.samueldanso@gmail.com>
2024-08-30 05:17:46 +0000

    Initial commit with 🏗️ Scaffold-ETH 2

---
+++ packages/nextjs/lib/networks.ts
@@ -0,0 +1,54 @@
+export const customEvmNetworks = [
+  {
+    blockExplorerUrls: ["https://optimistic.etherscan.io/"],
+    chainId: 10,
+    name: "OP Mainnet",
+    rpcUrls: ["https://mainnet.optimism.io"],
+    iconUrls: ["https://github.com/ethereum-optimism/brand-kit/blob/main/assets/images/Profile-Logo.png"],
+    nativeCurrency: {
+      name: "Ethereum",
+      symbol: "ETH",
+      decimals: 18,
+    },
+    networkId: 10,
+  },
+  {
+    blockExplorerUrls: ["https://sepolia.optimism.io"],
+    chainId: 11155420,
+    name: "OP Sepolia",
+    rpcUrls: ["https://sepolia-optimistic.etherscan.io"],
+    iconUrls: ["https://github.com/ethereum-optimism/brand-kit/blob/main/assets/images/Profile-Logo.png"],
+    nativeCurrency: {
+      name: "Ethereum",
+      symbol: "ETH",
+      decimals: 18,
+    },
+    networkId: 11155420,
+  },
+  {
+    blockExplorerUrls: ["https://blockscout.lisk.com"],
+    chainId: 1135,
+    name: "Lisk",
+    rpcUrls: ["https://rpc.api.lisk.com"],
+    iconUrls: ["https://cryptologos.cc/logos/lisk-lsk-logo.png"],
+    nativeCurrency: {
+      name: "Ethereum",
+      symbol: "ETH",
+      decimals: 18,
+    },
+    networkId: 1135,
+  },
+  {
+    blockExplorerUrls: ["	https://sepolia-blockscout.lisk.com"],
+    chainId: 4202,
+    name: "Lisk Sepolia Testnet",
+    rpcUrls: ["	https://rpc.sepolia-api.lisk.com"],
+    iconUrls: ["https://cryptologos.cc/logos/lisk-lsk-logo.png"],
+    nativeCurrency: {
+      name: "Ethereum",
+      symbol: "ETH",
+      decimals: 18,
+    },
+    networkId: 4202,
+  },
+];
